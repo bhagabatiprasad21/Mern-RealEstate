c37c4d228db9fc5ef78ecd20dfad1e43fdfa0e82
bhagabati prasad <bhagabati.prasad21@gmail.com>
2024-06-30 08:35:15 +0530

    create update user api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 const app = express();
 const  PORT = 8000;
 dotenv.config();
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 });
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
